3c8e34a5429181f4e998a29a4fcd42be2bb54177
RahulKhanSuvo <rahul.khan.suvo@gmail.com>
2025-11-25 11:37:02 +0600

    Connect AddNewClient form to API with phoneNumber and tag-style healthIssues input

--- src/hooks/useUserId.ts
+++ src/hooks/useUserId.ts
@@ -3,5 +3,5 @@ import { useAppSelector } from "./useRedux";
 
 export const useUserId = (): string | undefined => {
     const user = useAppSelector((state) => state.auth.user);
-    return user?.user?.id;
+    return user?.id;
 };
